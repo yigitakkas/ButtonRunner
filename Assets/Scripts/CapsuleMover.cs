3010fd4542b627e16f2ee100c4b7e5f5aa9d8534
Yiğit Akkaş <yigitakkasbjk@hotmail.com>
2025-01-07 17:03:28 +0300

    Add lateral movement and input handling to CapsuleMover:
    - Seperate lateral movement for mobile and PC platforms.

--- Assets/Scripts/CapsuleMover.cs
+++ Assets/Scripts/CapsuleMover.cs
@@ -2,10 +2,76 @@ using UnityEngine;
 
 public class CapsuleMover : MonoBehaviour
 {
-    public float MoveSpeed = 5f;
+    [Header("Movement Settings")]
+    public float ForwardSpeed = 4f;
+    public float LateralSpeed = 5f;
+    public float TouchSensitivity = 0.001f;
+    public float MinX = -3.7f;
+    public float MaxX = 3.7f;
+
+    private Vector2 _touchStartPosition;
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+        MoveForward();
+        HandleInput();
+    }
+
+    private void MoveForward()
+    {
+        transform.Translate(Vector3.forward * ForwardSpeed * Time.deltaTime);
+    }
+
+    private void HandleInput()
+    {
+        if (Application.isMobilePlatform)
+        {
+            HandleTouchInput();
+        }
+        else
+        {
+            HandleKeyboardInput();
+        }
+    }
+
+    private void HandleTouchInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    _touchStartPosition = touch.position;
+                    break;
+
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                    // Hareket miktarýný ekran geniþliðine göre normalize et
+                    float normalizedDeltaX = (touch.position.x - _touchStartPosition.x) / Screen.width;
+
+                    // Hareketi hassasiyete ve hýz katsayýsýna göre uygula
+                    float moveX = normalizedDeltaX * LateralSpeed * TouchSensitivity;
+
+                    MoveLateral(moveX);
+                    break;
+            }
+        }
+    }
+
+    private void HandleKeyboardInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        MoveLateral(horizontalInput * LateralSpeed * Time.deltaTime);
+    }
+
+    private void MoveLateral(float amount)
+    {
+        transform.Translate(Vector3.right * amount);
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, MinX, MaxX);
+        transform.position = clampedPosition;
     }
 }
